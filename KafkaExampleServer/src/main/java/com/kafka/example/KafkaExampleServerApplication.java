3f8411c556df43036fb1e00be7c523247d018a2d
HongSungMoon <hsm63746244@gmail.com>
2019-01-14 17:35:47 +0900

    add :  - initial commit  - web, kafka, kafka-stream , dev tool dependency 추가

---
+++ KafkaExampleServer/src/main/java/com/kafka/example/KafkaExampleServerApplication.java
@@ -0,0 +1,14 @@
+package com.kafka.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaExampleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaExampleServerApplication.class, args);
+	}
+
+}
+
